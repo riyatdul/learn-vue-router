122b6b2e1eef285925dafb9b6c229b57aae622ac
riyatdul <alriyatdul@gmail.com>
2024-10-09 09:37:26 +0700

    add/programic-route

---
+++ src/views/CreatePost.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+
+function submitForm() {
+    router.push({path: "/posts"})
+}
+</script>
+
+<template>
+    <h1>Create POST</h1>
+    <button @click="submitForm">Save</button>
+</template>
